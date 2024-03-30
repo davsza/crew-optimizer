af21cce201b8bc2b14bba0fa45d5f72dd136e6cb
davsza <david.szabolcs115@gmail.com>
2024-03-30 15:35:38 +0100

    Update model.mod and solver.py

--- backend/engine/model/model.mod
+++ backend/engine/model/model.mod
@@ -1,7 +1,7 @@
-set WORKERS ordered;  # Set of workers
-set SHIFTS ordered;     # Set of days
-set DAYS ordered;
-set NIGHTS ordered;
+set WORKERS ordered;	# Set of workers
+set SHIFTS ordered;	# Set of shifts
+set DAYS ordered;	# Set of days
+set NIGHTS ordered;	# Set of nights
 
 param min_workers{SHIFTS};	# Minimum required workers for each day
 param application{WORKERS, SHIFTS};	# Application for the workers
@@ -15,13 +15,13 @@ maximize Total_Match:
 subject to Work_Constraint {i in WORKERS}:
     sum {j in SHIFTS} x[i,j] = 5;
     
-# Constraint: each day a worker can work exactly 0 or 1 shift
+# Constraint: each day a worker can work exactly 0 or 1 shift each day
 subject to Max_One_Shift_Per_Day {i in WORKERS, j in DAYS}:
     sum {k in 1..3} x[i, (j - 1) * 3 + k] <= 1;
    
 #  Constraint: after the night shifts, the workers can't have any morning or afternoon shift
 subject to Night_Shifts {i in WORKERS, j in NIGHTS}:
-	sum {k in 1..3} x[i, 2 + (j - 1) * 3 + k] <= 1;
+	sum {k in 0..2} x[i, 3 + (j - 1) * 3 + k] <= 1;
 
 # Constraint: minimum required workers constraint
 subject to Min_Workers_Constraint {j in SHIFTS}:
--- backend/engine/solver.py
+++ backend/engine/solver.py
@@ -1,35 +1,38 @@
 from amplpy import AMPL
 import pandas as pd
-import numpy as np
-import os
 
 def prepare_data(N):
-    workers = [f"W{i:02d}" for i in range(1, N + 1)]
-    days = ["MON", "THU", "WED", "THR", "FRI", "SAT", "SUN"]
-    applications = [
-        [0, 0, 1, 1, 1, 1, 1],
-        [0, 0, 1, 1, 1, 1, 1],
-        [0, 0, 1, 1, 1, 1, 1],
-        [0, 0, 1, 1, 1, 1, 1],
-        [0, 0, 1, 1, 1, 1, 1]
-    ]
-    return workers, days, applications
+    workers = [f"W{i:02d}" for i in range(1, N + 1)] # getWorkerss()
+    days = [1, 2, 3, 4, 5, 6, 7] # getDays()
+    shifts = [(i + 1) for i in range(21)]
+    nights = [(i + 1) for i in range(6)]
+    applications = [[1] * 21 for _ in range(15)] # getApplications()
+    min_workers = [1] * 21
+    return workers, days, applications, shifts, nights, min_workers
 
-def shift_schedule(N: int):
+def model(N: int):
 
     model = AMPL()
     model.read("crew-optimizer/backend/engine/model/model.mod")
 
-    workers, days, applications = prepare_data(5)
-    # print(workers_dict)
-    # print(days_dict)
-    # print(applications)
+    workers, days, applications, shifts, nights, min_workers = prepare_data(N)
 
     model.set["WORKERS"] = workers
     model.set["DAYS"] = days
-    model.param["min_workers"] = [4, 4, 4, 4, 4, 2, 2]
-    model.param["application"] = pd.DataFrame(applications, index=workers, columns=days)
-    model.option["solver"] = "gurobi"
+    model.set["SHIFTS"] = shifts
+    model.set["NIGHTS"] = nights
+    model.param["min_workers"] = min_workers
+    model.param["application"] = pd.DataFrame(applications, index=workers, columns=shifts)
+    model.setOption('solver', ".\SCIPOptSuite\\bin\\scip.exe")
     model.solve()
 
-shift_schedule(5)
+    shift_matrix = model.getVariable("x")
+    data_dict = shift_matrix.getValues().toDict()
+    result = []
+    worker_ids = set(key[0] for key in data_dict)
+    for worker_id in sorted(worker_ids):
+        data = [data_dict.get((worker_id, i), 0) for i in range(1, 22)]
+        result.append(data)
+    print(result)
+
+model(15)
