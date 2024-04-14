8b246dec39b5b4016bfa5221dd5dd6a0b5de588b
davsza <david.szabolcs115@gmail.com>
2024-04-14 10:42:48 +0200

    Slight modifications

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -20,7 +20,6 @@ function Form({ route, method }) {
     try {
       const res = await api.post(route, { username, password });
       if (method === "login") {
-        console.log("login");
         localStorage.setItem(ACCESS_TOKEN, res.data.access);
         localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
         navigate("/");
